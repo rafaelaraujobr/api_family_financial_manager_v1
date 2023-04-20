06289c776665de84c276b0c7c09b2b384378c5d4
Rafael Araujo <rflaraujodev@gmail.com>
2023-04-20 19:01:09 -0300

    fix: cors 13

--- src/main.ts
+++ src/main.ts
@@ -55,6 +55,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
