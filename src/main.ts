89273ea52d92224eb1ea3205c615774aa5d83e57
Rafael Araujo <rflaraujodev@gmail.com>
2023-05-12 16:17:32 -0300

    feat: add description Api

--- src/main.ts
+++ src/main.ts
@@ -28,8 +28,29 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Api Fintrix')
     .setDescription(
-      `Fintrix é um aplicativo de controle financeiro para famílias que permite o monitoramento das despesas e receitas de todos os membros em um só lugar.<br> Ele oferece recursos para orçamento, divisão de contas e planejamento financeiro.
-       Com Fintrix, é fácil manter as finanças da família em ordem.`,
+      `<h1>Bem-vindo à API do Fintrix - Controle Financeiro Familiar!</h1>
+      <p>A API do Fintrix é uma poderosa ferramenta para auxiliar no controle financeiro de famílias. Com recursos abrangentes para gerenciar carteiras, estabelecer metas financeiras e registrar transações, o Fintrix oferece uma solução completa para manter suas finanças pessoais organizadas e sob controle.</p>
+      <h2>Principais recursos da API:</h2>
+      <ol>
+        <li>
+          <h3>Gerenciamento de Carteiras</h3>
+          <p>Crie e gerencie várias carteiras para acompanhar diferentes aspectos das suas finanças, como contas bancárias, investimentos e dinheiro em espécie. A API do Fintrix permite que você adicione, atualize, visualize e remova carteiras conforme necessário.</p>
+        </li>
+        <li>
+          <h3>Estabelecimento de Metas Financeiras</h3>
+          <p>Defina metas financeiras claras e alcançáveis para economizar dinheiro, pagar dívidas ou atingir outros objetivos financeiros importantes. Com a API do Fintrix, você pode criar metas, monitorar o progresso e receber atualizações sobre o desempenho em relação às metas definidas.</p>
+        </li>
+        <li>
+          <h3>Registro de Transações</h3>
+          <p>Registre suas transações financeiras, como despesas, receitas e transferências, para ter um registro completo das suas atividades financeiras. A API do Fintrix permite que você adicione detalhes relevantes, como a categoria da transação e a data, para melhor análise e rastreamento.</p>
+        </li>
+        <li>
+          <h3>Análise e Relatórios</h3>
+          <p>Acesse informações detalhadas sobre suas finanças por meio de recursos de análise e relatórios. Obtenha insights valiosos sobre seus gastos, receitas, tendências financeiras e muito mais, para uma compreensão completa da sua situação financeira.</p>
+        </li>
+      </ol>
+      <p>Com a API do Fintrix, você terá controle total sobre seu controle financeiro familiar. Seja você um desenvolvedor criando um aplicativo financeiro personalizado ou um usuário final que deseja integrar o Fintrix a outras ferramentas, nossa API oferece a flexibilidade e os recursos necessários para atender às suas necessidades.</p>
+      <p>Comece a explorar a API do Fintrix hoje mesmo e aproveite os benefícios de um controle financeiro eficiente e organizado para toda a sua família.</p>`,
     )
     .setVersion('1.0')
     .setTermsOfService('https://fintrix.com.br')
