38a7247d445fe907d0a4c775c525e4ce0ccef7f3
Rafael Araujo <rflaraujodev@gmail.com>
2023-05-12 16:10:41 -0300

    refactor: ui swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
 import { Get, Render } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { version } from '../package.json';
 @ApiTags('Home')
 @Controller()
 export class AppController {
+  @ApiOperation({ summary: 'Tela de apresentaçao da documentaçao' })
   @Get()
   @Render('index')
   root() {
--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ import * as basicAuth from 'express-basic-auth';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setBaseViewsDir(resolve('./src/views'));
+  app.useStaticAssets(resolve('./src/public'));
   app.setViewEngine('hbs');
   app.use(
     ['/api/v1/doc', '/api/v1/doc/*'],
@@ -25,9 +26,10 @@ async function bootstrap() {
   );
   //Swagger
   const config = new DocumentBuilder()
-    .setTitle('Api Fintrix V1')
+    .setTitle('Api Fintrix')
     .setDescription(
-      'Fintrix é um aplicativo de controle financeiro para famílias que permite o monitoramento das despesas e receitas de todos os membros em um só lugar. Ele oferece recursos para orçamento, divisão de contas e planejamento financeiro. Com Fintrix, é fácil manter as finanças da família em ordem.',
+      `Fintrix é um aplicativo de controle financeiro para famílias que permite o monitoramento das despesas e receitas de todos os membros em um só lugar.<br> Ele oferece recursos para orçamento, divisão de contas e planejamento financeiro.
+       Com Fintrix, é fácil manter as finanças da família em ordem.`,
     )
     .setVersion('1.0')
     .setTermsOfService('https://fintrix.com.br')
@@ -47,7 +49,19 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/v1/doc', app, document);
+  SwaggerModule.setup('api/v1/doc', app, document, {
+    customSiteTitle: 'Fintrix API - Swagger',
+    customCssUrl: '/swagger/theme-feeling-blue.css',
+    customCss: `
+    .topbar-wrapper a img {content: url("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png");} 
+    .swagger-ui .topbar {  background-color: #000000}
+    .scheme-container {background-color: #fafafa !important;}`,
+    customfavIcon: '/swagger/favicon.ico',
+    swaggerOptions: {
+      docExpansion: 'none',
+      showRequestDuration: true,
+    },
+  });
   app.useGlobalInterceptors(new LoggerInterceptor());
   app.enableCors({
     origin: '*',
@@ -56,5 +70,6 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With, X-Refresh-Token',
   });
   await app.listen(process.env.PORT || 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
